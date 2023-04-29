e6d5feef1168f3ea83bb74dbbe6c33cc66e06076
Natzaum <natacezerbordignon@gmail.com>
2023-04-29 02:49:42 -0300

    as rotas da API

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
-const animeController = require("./controllers/animeController")
+const animeController = require("./controllers/animeController");
+const animeMiddleware = require("./middlewares/animesmiddleware");
 
 const router = express.Router();
 
-router.get("/animes", animeController.getAll)
+router.get("/animes", animeController.getAll);
+router.post("/animes", animeMiddleware.validatebody, animeController.createAnime);
+router.delete("/animes/:id", animeController.deleteAnime);
+router.put("/animes/:id", animeMiddleware.validatebody, animeController.updateAnime);
 
 module.exports = router;
